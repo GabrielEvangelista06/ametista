15a604d32a50b593bd8546b4e3b1fca19573f3c4
Gabriel Evangelista <gabrielevangelista094@gmail.com>
2024-04-02 23:30:48 -0300

    style: adding py-4 if the screen is bigger than 2xl

--- src/app/app/transactions/_components/TransactionsDataTable.tsx
+++ src/app/app/transactions/_components/TransactionsDataTable.tsx
@@ -400,7 +400,7 @@ export function TransactionsDataTable({ data }: TransactionsDataTableProps) {
         </Table>
       </div>
       <div
-        className={`flex items-center justify-end space-x-2 ${table.getFilteredRowModel().rows.length > 10 ? 'py-0 pt-1' : 'py-4'}`}
+        className={`flex items-center justify-end space-x-2 2xl:py-4 ${table.getFilteredRowModel().rows.length > 10 ? 'py-0 pt-1' : 'py-4'}`}
       >
         <div className="flex-1 text-sm text-muted-foreground">
           {table.getFilteredSelectedRowModel().rows.length} de{' '}
