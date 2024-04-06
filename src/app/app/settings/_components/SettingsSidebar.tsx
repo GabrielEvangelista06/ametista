9536f02377b87de22e56821bca33d1a467c6a72f
Gabriel Evangelista <gabrielevangelista094@gmail.com>
2024-04-06 00:05:40 -0300

    feat: creating the settings sidebar

---
+++ src/app/app/settings/_components/SettingsSidebar.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+import {
+  DashboardSidebar,
+  DashboardSidebarMain,
+  DashboardSidebarNav,
+  DashboardSidebarNavLink,
+} from '@/components/layouts/Sidebar'
+
+export function SettingsSidebar() {
+  const pathname = usePathname()
+
+  const isActive = (path: string) => pathname === path
+
+  return (
+    <DashboardSidebar>
+      <DashboardSidebarNav>
+        <DashboardSidebarMain>
+          <DashboardSidebarNavLink
+            href="/app/settings"
+            index={1}
+            active={isActive('/app/settings')}
+          >
+            Meu perfil
+          </DashboardSidebarNavLink>
+          <DashboardSidebarNavLink
+            href="/app/settings/appearance"
+            index={2}
+            active={isActive('/app/settings/appearance')}
+          >
+            Aparência
+          </DashboardSidebarNavLink>
+          <DashboardSidebarNavLink
+            href="/app/settings/subscription"
+            index={3}
+            active={isActive('/app/settigs/subscription')}
+          >
+            Assinatura
+          </DashboardSidebarNavLink>
+        </DashboardSidebarMain>
+      </DashboardSidebarNav>
+    </DashboardSidebar>
+  )
+}
