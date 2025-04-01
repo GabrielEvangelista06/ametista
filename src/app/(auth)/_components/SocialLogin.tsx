14b14094f4ca804ae60a887c2c9d9d6be4587990
Gabriel Evangelista <gabrielevangelista094@gmail.com>
2025-04-01 15:09:29 -0300

    fix: use environment variable for Google sign-in callback URL

--- src/app/(auth)/_components/SocialLogin.tsx
+++ src/app/(auth)/_components/SocialLogin.tsx
@@ -13,7 +13,7 @@ export function SocialLogin({ labelGoogle }: SocialLoginProps) {
   const loginWithGoogle = async () => {
     try {
       setIsLoading(true)
-      const callbackUrl = 'https://ametista-nextjs.3vbbk5.easypanel.host/app'
+      const callbackUrl = process.env.NEXT_PUBLIC_CALLBACK_URL
       await signIn('google', { callbackUrl })
     } finally {
       setIsLoading(false)
